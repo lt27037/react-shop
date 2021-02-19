7db75ee073b2011334a5d4be0f99c6d0691c7edc
Łukasz Tukindorf <lukasz.tukindorf@gmail.com>
2021-02-19 11:28:17 +0100

    instal node-sass and create layouts files

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from 'react';
 
+import ContentContainer from './layouts/ContentContainer';
+import Footer from './layouts/Footer';
+import Header from './layouts/Header';
+
+import './styles/App.scss';
+
 const App = () => {
    return(
-      <div>XD</div>
+      <div id="appContainer">
+         <Header />
+         <ContentContainer />
+         <Footer />
+      </div>
    )
 }
 
